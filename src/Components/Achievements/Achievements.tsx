78c6362d30fb746077beccbc1ce21ef86ef95e7d
brandencall <brandencall@live.com>
2025-04-01 12:05:53 -0600

    Updated Achievements

--- src/Components/Achievements/Achievements.tsx
+++ src/Components/Achievements/Achievements.tsx
@@ -2,33 +2,31 @@ import AchievmentCard from "../../Components/Achievements/AchievementCard.tsx";
 import "./Achievements.css";
 
 function Achievements() {
-    const PDF = "/BLS.pdf";
-    const HeadShot = "/ChristopherClarkHeadShot.JPEG";
     return (
         <div className="achievement-grid-container">
-            <a className="pdf-link" href={PDF} target="_blank">
+            <a className="pdf-link" href="/BLS.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image={HeadShot} title="Title of Cert" size={300} />
+                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href={PDF} target="_blank">
+            <a className="pdf-link" href="/BLS.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image={HeadShot} title="Title of Cert" size={300} />
+                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href={PDF} target="_blank">
+            <a className="pdf-link" href="/BLS.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image={HeadShot} title="Title of Cert" size={300} />
+                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href={PDF} target="_blank">
+            <a className="pdf-link" href="/BLS.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image={HeadShot} title="Title of Cert" size={300} />
+                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href={PDF} target="_blank">
+            <a className="pdf-link" href="/BLS.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image={HeadShot} title="Title of Cert" size={300} />
+                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
                 </div>
             </a>
         </div>
