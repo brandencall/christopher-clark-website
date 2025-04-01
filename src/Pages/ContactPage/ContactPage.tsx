22eff2b4df8972c5874452540a9657e8b6132d24
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-01 17:48:49 -0600

    Update ContactPage.tsx

--- src/Pages/ContactPage/ContactPage.tsx
+++ src/Pages/ContactPage/ContactPage.tsx
@@ -22,10 +22,10 @@ function ContactPage() {
                     <p className="body-text"><FaGlobeAmericas className="body-logo" />Languages</p>
                 </div>
                 <div className="body-right-container">
-                    <p className="body-text">Albuquerque, New Mexico</p>
                     <p className="body-text">christophersjgrclark@gmail.com</p>
-                    <p className="body-text">University of New Mexico (UNM), Liberty University, and Champlain College</p>
                     <p className="body-text">Call: (915)291-7016 Text: (915)247-8178</p>
+                    <p className="body-text">Albuquerque, New Mexico</p>
+                    <p className="body-text">University of New Mexico (UNM), Liberty University, and Champlain College</p>
                     <p className="body-text">English</p>
                 </div>
             </div>
