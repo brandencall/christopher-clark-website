8a18626e21f671fadf1f15ff13ac3319f7ca277b
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-02 12:36:45 -0600

    .

--- src/Components/Achievements/Achievements.tsx
+++ src/Components/Achievements/Achievements.tsx
@@ -4,39 +4,139 @@ import "./Achievements.css";
 function Achievements() {
     return (
         <div className="achievement-grid-container">
+            <a className="pdf-link" href="/PMP-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/PMP-IMG.png" title="Project Management Professional (PMP)" size={300} />
+                </div>
+            </a>
+            <a className="pdf-link" href="/ITIL-Foundation-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/ITIL-Foundation-IMG.png" title="ITIL® v4 Foundation" size={300} />
+                </div>
+            </a>
+            <a className="pdf-link" href="/CSM-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/CSM-IMG.png" title="Certified Scrum Master (CSM)" size={300} />
+                </div>
+            </a>
             <a className="pdf-link" href="/CISM-Cert.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image="/CISM-IMG.png" title="CRISC" size={300} />
-                </div>
-            </a>
-            <a className="pdf-link" href="/CRISC-Cert.pdf" target="_blank">
-                <div className="achievement-grid-item">
-                    <AchievmentCard image="/CRISC-IMG.png" title="CRISC" size={300} />
-                </div>
-            </a>
-            <a className="pdf-link" href="/CRISC-Cert.pdf" target="_blank">
-                <div className="achievement-grid-item">
-                    <AchievmentCard image="/CRISC-IMG.png" title="CRISC" size={300} />
-                </div>
-            </a>
-            <a className="pdf-link" href="/CRISC-Cert.pdf" target="_blank">
-                <div className="achievement-grid-item">
-                    <AchievmentCard image="/CRISC-IMG.png" title="CRISC" size={300} />
+                    <AchievmentCard image="/CISM-IMG.png" title="Certified Information Security Manager (CISM)" size={300} />
                 </div>
             </a>
             <a className="pdf-link" href="/CISA-Cert.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image="/CISA-IMG.png" title="CISA" size={300} />
+                    <AchievmentCard image="/CISA-IMG.png" title="Certified Information Systems Auditor (CISA)" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href="/PMP-Cert.pdf" target="_blank">
+            <a className="pdf-link" href="/CRISC-Cert.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image="/PMP-IMG.png" title="PMP" size={300} />
+                    <AchievmentCard image="/CRISC-IMG.png" title="Certified in Risk & Information Systems Control (CRISC)" size={300} />
                 </div>
             </a>
-            <a className="pdf-link" href="/BLS.pdf" target="_blank">
+            <a className="pdf-link" href="/SecurityX-Cert.pdf" target="_blank">
                 <div className="achievement-grid-item">
-                    <AchievmentCard image="/ChristopherClarkHeadShot.JPEG" title="Title of Cert" size={300} />
+                    <AchievmentCard image="/SecurityX-IMG.png" title="SecurityX (Formerly known as CASP+)" size={300} />
+                </div>
+            </a>
+            <a className="pdf-link" href="/CySA+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/CySA+-IMG.png" title="Cybersecurity Analyst (CySA+)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/PenTest+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/PenTest+-IMG.png" title="PenTest+" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Security+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Security+-IMG.png" title="Security+" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Linux+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Linux+-IMG.png" title="Linux+" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Network+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Network+-IMG.png" title="Network+" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/A+-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/A+-IMG.png" title="A+" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Microsoft-Azure-Architect-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Microsoft-Azure-Architect-IMG.png" title="Azure Solutions Architect Expert" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Microsoft-Azure-Admin-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Microsoft-Azure-Admin-IMG.png" title="Azure Administrator Associate" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/JNCIA-SEC-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/JNCIA-SEC-IMG.png" title="Juniper Networks Certified Associate - Security (JNCIA-SEC)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/JNCIA-DC-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/JNCIA-DC-IMG.png" title="Juniper Networks Certified Associate - Data Center (JNCIA-DC)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/CEH-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/CEHC-IMG.png" title="Certified Ethical Hacker (CEH)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/CHFI-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/CHFI-IMG.png" title="Computer Hacking Forensic Investigator (CHFI)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/CTIA-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/CTIA-IMG.png" title="EC-Council Certified Threat Intelligence Analyst (CTIA)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/ECIH-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/ECIH-IMG.png" title="Certified Incident Handler (ECIH)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Cisco-CCNP-Security-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Cisco-CCNP-Security-IMG.png" title="Cisco Certified Network Professional Security (CCNP-S) (Expired)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Cisco-Network-Security-Firepower-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Cisco-Network-Security-Firepower-IMG.png" title="Cisco Certified Network Security Firepower (Expired)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Cisco-Security-Core-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Cisco-Security-Core-IMG.png" title="Cisco Certified Security Core (Expired)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Cisco-CyberOps-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Cisco-CyberOps-IMG.png" title="Cisco Certified Network Associate Cyber Ops (Expired)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/Cisco-CCNA-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/Cisco-CCNA-IMG.png" title="Cisco Certified Network Associate Security (CCNA) (Expired)" size={300} />
+                </div>
+                </a>
+            <a className="pdf-link" href="/OCA-Cert.pdf" target="_blank">
+                <div className="achievement-grid-item">
+                    <AchievmentCard image="/OCA-IMG.png" title="Oracle Certified Associate (OCA), Java SE 8 Programmer (Expired)" size={300} />
                 </div>
             </a>
         </div>
