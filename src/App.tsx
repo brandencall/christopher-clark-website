40c6a2b7148eaf99ababcfb2b11e0593e3e34671
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-03 13:13:32 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
+import BackgroundPage from "./Pages/BackgroundPage/BackgroundPage"; // ✅ new import
 import SkillsPage from "./Pages/SkillsPage/SkillsPage";
 import ContactPage from "./Pages/ContactPage/ContactPage";
 import Navbar from "./Components/Navbar";
 
 function App() {
-
     return (
         <>
             <Router>
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="/Skills" element={<SkillsPage />} />
-                    <Route path="/Contact" element={<ContactPage/>} />
+                    <Route path="/background" element={<BackgroundPage />} /> {/* ✅ new route */}
+                    <Route path="/skills" element={<SkillsPage />} />
+                    <Route path="/contact" element={<ContactPage />} />
                 </Routes>
             </Router>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
