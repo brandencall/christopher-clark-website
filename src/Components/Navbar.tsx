488c77b3880b03d4bcd45cd8fbe50af5a6583ff5
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-05 14:59:42 -0600

    .

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-        <div className="nav-container">
-                <ul className="nav-links">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/Background">Background</Link></li>
-                    <li><Link to="/Projects">Projects</Link></li>
-                    <li><Link to="/Skills">Skills</Link></li>
-                    <li><Link to="/Contact">Contact</Link></li>
-                </ul>
+      <div className="nav-container">
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <span />
+          <span />
+          <span />
         </div>
+        <ul className={`nav-links ${isMenuOpen ? "open" : ""}`}>
+          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+          <li><Link to="/Background" onClick={closeMenu}>Background</Link></li>
+          <li><Link to="/Projects" onClick={closeMenu}>Projects</Link></li>
+          <li><Link to="/Skills" onClick={closeMenu}>Skills</Link></li>
+          <li><Link to="/Contact" onClick={closeMenu}>Contact</Link></li>
+        </ul>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
