675caa9bb259ca2f13bc00b6a41790b9f3597741
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-05 15:31:39 -0600

    .

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,34 +1,15 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <ul className="nav-group left">
-          <li><Link to="/" onClick={closeMenu}>Home</Link></li>
-          <li><Link to="/Background" onClick={closeMenu}>Background</Link></li>
-        </ul>
-
-        <ul className="nav-group center">
-          <li><Link to="/Projects" onClick={closeMenu}>Projects</Link></li>
-        </ul>
-
-        <ul className="nav-group right">
-          <li><Link to="/Skills" onClick={closeMenu}>Skills</Link></li>
-          <li><Link to="/Contact" onClick={closeMenu}>Contact</Link></li>
-        </ul>
+        <div className="nav-item"><Link to="/">Home</Link></div>
+        <div className="nav-item"><Link to="/Background">Background</Link></div>
+        <div className="nav-item"><Link to="/Projects">Projects</Link></div>
+        <div className="nav-item"><Link to="/Skills">Skills</Link></div>
+        <div className="nav-item"><Link to="/Contact">Contact</Link></div>
       </div>
     </nav>
   );
