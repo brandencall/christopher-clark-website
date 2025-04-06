61dee34450a7bda2d07cda8ba694faed54e09e1e
Christopher Clark <90009654+Christopher-D-Clark@users.noreply.github.com>
2025-04-05 19:19:40 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import BackgroundPage from "./Pages/BackgroundPage/BackgroundPage";
