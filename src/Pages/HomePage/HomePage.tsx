4aeb8390474e01f0532b4ed21277ba42091678cb
brandencall <brandencall@live.com>
2025-04-13 16:57:26 -0600

    Fixed icons on HomePage for mobile.

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -11,10 +11,10 @@ function HomePage() {
 
     return (
         <div className={`page-content ${fadeIn ? "fade-in" : ""}`}>
+            <div className="icon-row fade-in-up" style={{ animationDelay: '0.5s' }}>
+                <ContactIcons />
+            </div>
             <div className="container">
-                <div className="icon-row fade-in-up" style={{ animationDelay: '0.5s' }}>
-                    <ContactIcons />
-                </div>
     
                 <img
                 src="/ChristopherClarkHeadShot.JPEG"
